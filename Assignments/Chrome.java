d2842813fd5e5b3fe7d306826aa5a052a4f84a30
SabtharVeera <sabtharveera18@gmail.com>
2025-04-27 21:37:37 +0530

    Add files via upload
    Day1 Assignments

---
+++ Assignments/Chrome.java
@@ -0,0 +1,26 @@
+package Assignments;
+
+public class Chrome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		float version=91.0f;
+		System.out.println(version);
+		
+		String Developer = "Google";
+		System.out.println(Developer);
+		
+		boolean Beta = false;
+		System.out.println(Beta);
+		
+		int releaseYear = 2008;
+		System.out.println(releaseYear);
+		
+		char shortCutKey = 'C';
+		System.out.println(shortCutKey);
+		
+
+	}
+
+}
